b344c31454debaa41d7610b06cd5e3b55812c9d5
Marisa Clardy <marisa@clardy.eu>
2019-10-25 19:24:15 -0500

    Handle GitData object instead of arrays.

--- resources/views/snippets/contrib-calendar.blade.php
+++ resources/views/snippets/contrib-calendar.blade.php
@@ -1,8 +1,6 @@
-@if (!isset($git))
-    @inject ('git', 'App\Services\GithubApi')
-@endif
-<span class="text-gray-600">Earliest Date: {{ $gitData['earliest_date']->toDateString() }}</span>
-<span class="text-gray-600 float-right">Latest Date: {{ $gitData['latest_date']->toDateString() }}</span>
+
+<span class="text-gray-600">Earliest Date: {{ $gitData->earliest_date->toDateString() }}</span>
+<span class="text-gray-600 float-right">Latest Date: {{ $gitData->latest_date->toDateString() }}</span>
 <table class="calendar-heatmap">
     <tbody>
         @for ($dayOfWeek = 1; $dayOfWeek <= 7; $dayOfWeek++)
@@ -10,32 +8,32 @@
                 @for ($weekNumber = 0; $weekNumber <= 53; $weekNumber++)
                     @if ($weekNumber > 0)
                     @php
-                    $curDay = Carbon\Carbon::parse($gitData['earliest_date']->copy()->startOfWeek()->format('Y-m-d'))
+                    $curDay = Carbon\Carbon::parse($gitData->earliest_date->copy()->startOfWeek()->format('Y-m-d'))
                         ->addDays($dayOfWeek-2)->addWeeks($weekNumber-1);
-                    $diff = $curDay->diffInDays($gitData['earliest_date']);
+                    $diff = $curDay->diffInDays($gitData->earliest_date);
 
-                    $daysEvents = optional($gitData['data'][$dayOfWeek])[$diff] ?? [
+                    $daysEvents = optional($gitData->data[$dayOfWeek])[$diff] ?? [
                         'date' => $curDay,
                         'count' => 'No',
                         'heatmap_class' => 'bg-gray-300'
                     ];
                     @endphp
                     <td class="bg-gray-100 border-solid border-gray-100 border-2" title="{{ $daysEvents['count'] }} contributions on {{ $daysEvents['date']->toDateString() }}">
-                        @if($daysEvents['date']->lt($gitData['earliest_date']) || $daysEvents['date']->gt($gitData['latest_date']))
+                        @if($daysEvents['date']->lt($gitData->earliest_date) || $daysEvents['date']->gt($gitData->latest_date))
                         <svg width=15 height=15>
                             <rect x=0 y=0 width=15 height=15 fill="none"></rect>
                         </svg>
                         @else
-                        <svg width=15 height=15 class="{{ $git->determineHeatmapColour($daysEvents['count']) }} rounded-full">
+                        <svg width=15 height=15 class="{{ App\GitData::determineHeatmapColour($daysEvents['count']) }} rounded-full">
                             <rect x=0 y=0 width=15 height=15 fill="none"></rect>
                         </svg>
                         @endif
                         <span style="display: none" name="debug">
                             <span>{{ $daysEvents['count'] }} contributions</span><br>
                             <span>{{ $daysEvents['date']->toDateString() }}</span><br>
-                            <span>Diff: {{ $diff }}; AddedDays: {{ $gitData['earliest_date']->copy()->addDays($diff)->toDateString() }}</span><br>
+                            <span>Diff: {{ $diff }}; AddedDays: {{ $gitData->earliest_date->copy()->addDays($diff)->toDateString() }}</span><br>
                             <span>CurDate: {{ $curDay }}</span><br>
-                            <span>Earliest: {{ $gitData['earliest_date']->toDateString() }}</span>
+                            <span>Earliest: {{ $gitData->earliest_date->toDateString() }}</span>
                         </span>
                     </td>
                     @else
@@ -58,27 +56,27 @@
                 &nbsp;
             </td>
             <td class="bg-gray-100 border-solid border-gray-100 border-2" title="No contributions">
-                <svg width=15 height=15 class="{{ $git->determineHeatmapColour(0) }} rounded-full">
+                <svg width=15 height=15 class="{{ App\GitData::determineHeatmapColour(0) }} rounded-full">
                     <rect x=0 y=0 width=15 height=15 fill="none"></rect>
                 </svg>
             </td>
             <td class="bg-gray-100 border-solid border-gray-100 border-2" title="1-9 contributions">
-                <svg width=15 height=15 class="{{ $git->determineHeatmapColour(3) }} rounded-full">
+                <svg width=15 height=15 class="{{ App\GitData::determineHeatmapColour(3) }} rounded-full">
                     <rect x=0 y=0 width=15 height=15 fill="none"></rect>
                 </svg>
             </td>
             <td class="bg-gray-100 border-solid border-gray-100 border-2" title="10-19 contributions">
-                <svg width=15 height=15 class="{{ $git->determineHeatmapColour(13) }} rounded-full">
+                <svg width=15 height=15 class="{{ App\GitData::determineHeatmapColour(13) }} rounded-full">
                     <rect x=0 y=0 width=15 height=15 fill="none"></rect>
                 </svg>
             </td>
             <td class="bg-gray-100 border-solid border-gray-100 border-2" title="20-29 contributions">
-                <svg width=15 height=15 class="{{ $git->determineHeatmapColour(23) }} rounded-full">
+                <svg width=15 height=15 class="{{ App\GitData::determineHeatmapColour(23) }} rounded-full">
                     <rect x=0 y=0 width=15 height=15 fill="none"></rect>
                 </svg>
             </td>
             <td class="bg-gray-100 border-solid border-gray-100 border-2" title="30+ contributions">
-                <svg width=15 height=15 class="{{ $git->determineHeatmapColour(33) }} rounded-full">
+                <svg width=15 height=15 class="{{ App\GitData::determineHeatmapColour(33) }} rounded-full">
                     <rect x=0 y=0 width=15 height=15 fill="none"></rect>
                 </svg>
             </td>
