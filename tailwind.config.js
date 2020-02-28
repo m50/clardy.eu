2cd91ae88bb608905ae6a2384a7d701d33a71a31
Marisa Clardy <marisa@clardy.eu>
2020-02-27 20:48:57 -0600

    Fix bug where a certain font causes print page to break in chrome.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,23 @@
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'print': {'raw': 'print'},
+      }
+    },
+    fontFamily: {
+      sans: [
+        'system-ui', "-apple-system", "Segoe UI", 'Roboto', "Helvetica Neue",
+        'Arial', "Noto Sans", 'sans - serif', "Apple Color Emoji",
+        "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"
+      ],
+      serif: [
+        'Georgia', 'Cambria', "Times New Roman", 'Times', 'serif'
+      ],
+      mono: [
+        'Menlo', 'Monaco', 'Consolas', "Liberation Mono", "Courier New", 'monospace'
+      ],
+    },
   },
   variants: {},
   plugins: [],
