20dcce39d8a19f76097c2977f83016df8e8b39ef
Marisa Clardy <marisa@clardy.eu>
2021-02-28 16:54:11 -0600

    Some simplifications.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,18 +5,18 @@ import { Header as HeaderRender } from './Header';
 
 const Header: React.FC = () => (
   <StaticQuery
-      query={graphql`
-        query HeaderQuery {
-          allSitePage(filter: {componentPath: {regex: "/^.*(?<!PostLayout\\.jsx)$/"}, path: {regex: "/^\\/(?!(dev-)?404).*\\/$/"}}, sort: {fields: internalComponentName, order: ASC}) {
-            nodes {
-              id
-              path
-              internalComponentName
-            }
+    query={graphql`
+      query HeaderQuery {
+        allSitePage(filter: {componentPath: {regex: "/^.*(?<!PostLayout\\.jsx)$/"}, path: {regex: "/^\\/(?!(dev-)?404).*\\/$/"}}, sort: {fields: internalComponentName, order: ASC}) {
+          nodes {
+            id
+            path
+            internalComponentName
           }
-        }`}
-      render={(data: Data) => <HeaderRender data={data} />}
-    />
+        }
+      }`}
+    render={(data: Data) => <HeaderRender data={data} />}
+  />
 );
 
 export default Header;
