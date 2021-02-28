8c04b8bcc80dbf9648ee6144f587bf0ca4d0aa16
Marisa Clardy <marisa@clardy.eu>
2021-02-28 15:04:43 -0600

    Simplify Social component.

--- src/components/Social/index.tsx
+++ src/components/Social/index.tsx
@@ -5,24 +5,18 @@ import Instagram from "./Instagram";
 import Twitter from './Twitter';
 import DevTo from './DevTo';
 
-interface Props { }
-
-const socials = [
-  () => <Twitter />,
-  () => <Github />,
-  () => <Gitlab />,
-  () => <Instagram />,
-  () => <DevTo />,
+const socials: JSX.Element[] = [
+  <Twitter />,
+  <Github />,
+  <Gitlab />,
+  <Instagram />,
+  <DevTo />,
 ];
 
-const Social: React.FunctionComponent<Props> = (_props: Props) => {
+const Social: React.FC = () => {
   return (
     <ul className="print:hidden flex-grow flex justify-between py-3 h-5 content-center items-center">
-      {socials.map((SocialLink, idx: number) => (
-        <li className="list-none" key={idx}>
-          {SocialLink()}
-        </li>
-      ))}
+      {socials.map((S, idx: number) => <li className="list-none" key={idx}>{S}</li>)}
     </ul>
   )
 }
