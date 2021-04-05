485f736f87101702bfd04e200e58de10623a4f0b
Marisa Clardy <marisa@clardy.eu>
2021-04-04 20:14:47 -0500

    Use slightly thicker border.

--- src/components/styled/NavLink.tsx
+++ src/components/styled/NavLink.tsx
@@ -3,5 +3,5 @@ import tw from 'tailwind-styled-components';
 export const NavLink = tw.a`
   pt-12 text-white text-3xl font-subheader uppercase
   h-full tracking-wide cursor-pointer border-yellow-400
-  hover:underline hover:border-b-2
+  hover:underline hover:border-b-4
 `;
